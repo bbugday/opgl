d5b8fe1e638bfb746e8dca1c4afc4cdb89c987f7
berke <bugdayberke@gmail.com>
2021-07-18 02:25:41 +0300

    texture unit

--- opgl1/src/main.cpp
+++ opgl1/src/main.cpp
@@ -65,6 +65,7 @@ int main(void)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load and generate the texture
     int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
     unsigned char* data = stbi_load("res/textures/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -75,12 +76,38 @@ int main(void)
     {
         std::cout << "Failed to load texture" << std::endl;
     }
+
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    data = stbi_load("res/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
     stbi_image_free(data);
 
     vao.unbind();
     vbo.unbind();
     ebo.unbind();
     glBindTexture(GL_TEXTURE_2D, 0);
+    
+    tShader.use();
+    tShader.setInt("texture1", 0);
+    tShader.setInt("texture2", 1);
 
     while (!window.shouldClose())
     {
@@ -88,7 +115,10 @@ int main(void)
 
         tShader.use();
 
+        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         vao.bind();
 
